e79919afc089a5e2e022081ae575c22f5f9e55c5
Moez Bouhlel <lejenome@tik.tn>
2020-04-08 09:41:50 +0100

    fix django support

--- django_hstore/widgets.py
+++ django_hstore/widgets.py
@@ -1,11 +1,10 @@
-from __future__ import absolute_import, unicode_literals
-
-import django
-from django import forms
+try:
+    from django.contrib.admin.templatetags.admin_static import static
+except ImportError:
+    from django.templatetags.static import static
 from django.conf import settings
-from django.contrib.admin.templatetags.admin_static import static
 from django.contrib.admin.widgets import AdminTextareaWidget
-from django.template import Context
+from django.contrib.postgres.forms import forms
 from django.template.loader import get_template
 from django.utils.safestring import mark_safe
 
@@ -31,7 +30,7 @@ class BaseAdminHStoreWidget(AdminTextareaWidget):
 
         return forms.Media(js=js)
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         if attrs is None:
             attrs = {}
         # it's called "original" because it will be replaced by a copy
@@ -41,15 +40,14 @@ class BaseAdminHStoreWidget(AdminTextareaWidget):
         html = super(BaseAdminHStoreWidget, self).render(name, value, attrs)
 
         # prepare template context
-        template_context = Context({
+        template_context = {
             'field_name': name,
-            'STATIC_URL': settings.STATIC_URL,
-            'use_svg': django.VERSION >= (1, 9),  # use svg icons if django >= 1.9
-        })
+        }
+
         # get template object
         template = get_template('hstore_%s_widget.html' % self.admin_style)
         # render additional html
-        additional_html = template.render(template_context)
+        additional_html = template.render(context=template_context)
 
         # append additional HTML and mark as safe
         html = html + additional_html
